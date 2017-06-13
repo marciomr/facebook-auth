8a57096402c820800e761db5d07e5cfb56eada65
Marcio M. Ribeiro <marcio.mr@gmail.com>
2017-06-13 18:30:47 -0300

    criei um botão de like na página

--- index.php
+++ index.php
@@ -3,8 +3,31 @@
   <head>
     <meta charset="utf-8">
     <title>Autorização para o Monitor</title>
+    <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId      : '674584949359563',
+        xfbml      : true,
+        version    : 'v2.9'
+      });
+      FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
   </head>
   <body>
-    
+    <div
+      class="fb-like"
+      data-share="true"
+      data-width="450"
+      data-show-faces="true">
+    </div>
   </body>
 </html>
